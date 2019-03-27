fedbbb5121f3c063054d42f214c987d835e373cf
Tommy1856 <43572840+Tommy1856@users.noreply.github.com>
2019-03-27 12:11:09 +0000

    Add files via upload

--- js/UIScene.js
+++ js/UIScene.js
@@ -12,11 +12,17 @@ class UIScene extends BaseScene{
   create() {
     //VirtualJoystick
     this.stick = this.pad.addStick(120, 1075, 200, 'joystickcontroller');
+
     //this.stick.setScrollFactor(0);
     this.stick.on('update', stickUpdate, this);
     this.stick.scale = 1.0;
+
+    //Loading Scene/Character
     this.gameScene = this.scene.manager.getScene('TownScene');
     this.gameArcher = this.gameScene.archer.character.body;
+
+    //Fire Button
+  //  this.fireButton = this.pad.addButton(1300, 1075, 100, 'joystickcontroller', 'button1-up', 'button1-down');
   }
 
   update(){
@@ -37,5 +43,10 @@ function stickUpdate (stick, force)
   else{
     this.gameArcher.velocity.set(0);
   }
-
 }
+
+/* function buttonUpdate(){
+  if (button.isDown){
+
+  }
+} */
